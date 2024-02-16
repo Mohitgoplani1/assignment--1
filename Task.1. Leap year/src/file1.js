641cc39fe0469498ddedb7f1ea163a34c40693f6
Mohit Goplani <goplanimohit510@gmail.com>
2024-02-16 22:57:37 +0530

    first task

---
+++ Task.1. Leap year/src/file1.js
@@ -0,0 +1,5 @@
+import { isLeapYear } from './dateUtil.js'; 
+let x = isLeapYear(2020);
+console.log(x);
+let x1 = isLeapYear(1900);
+console.log(x1);
---
+++ Task.1. Leap year/src/file2.js
@@ -0,0 +1,3 @@
+import myUtil from './dateUtil.js'; 
+console.log( myUtil.isLeapYear(2020));
+console.log( myUtil.isLeapYear(1900));
